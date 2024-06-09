9300598e6deb952a0c4d5edb355e918dde74ff7b
Suvrat344 <chauhansuvrat344@gmail.com>
2024-06-09 19:05:47 +0530

    Data Types Variables And Input

---
+++ 12DataTypesVariablesAndInput/p1Pattern.java
@@ -0,0 +1,22 @@
+// Show the following patterns using single print statement
+/*
+ *                  * * * * *                    *              
+ * *                  * * * *                  * * 
+ * * *                  * * *                * * *   
+ * * * *                  * *              * * * *
+ * * * * *                  *            * * * * *
+ */
+
+
+ public class p1Pattern {
+  public static void main(String[] args) {
+      System.out.println("-------------Pattern1----------");
+      System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+
+      System.out.println("-------------Pattern2----------");
+      System.out.println("* * * * *\n* * * *\n* * *\n* *\n*");
+
+      System.out.println("-------------Pattern3----------");
+      System.out.println("        *\n      * *\n    * * *\n  * * * *\n* * * * *");
+  }
+}
