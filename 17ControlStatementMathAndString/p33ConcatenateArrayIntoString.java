68bf6a8e861611f7a68c903c6bb340bd135c4e76
Suvrat344 <chauhansuvrat344@gmail.com>
2024-06-22 10:23:21 +0530

    Control Structure,Math And String Question

--- 17ControlStatementMathAndString/p33ConcatenateArrayIntoString.java
+++ 17ControlStatementMathAndString/p33ConcatenateArrayIntoString.java
@@ -1,24 +1,16 @@
 // Take an array of words and concatenate them into a single string using StringBuilder.
 
-import java.util.Scanner;
-
 public class p33ConcatenateArrayIntoString
 {
   public static void main(String[] args) 
   {
+
+      String[] arr = new String[] {"This"," is"," a"," java"," folder."};
       StringBuilder sentence = new StringBuilder();
-      String word;
 
-      Scanner input = new Scanner(System.in);
-
-      System.out.print("How many words you want to enter? : ");
-      int counter = input.nextInt();
-
-      for(int i = 0;i < counter; i++)
+      for(int i = 0;i < arr.length; i++)
       {
-        System.out.print("Enter word : ");
-        word = input.next();
-        sentence.append(" " + word);
+        sentence.append(arr[i]);
       }
       System.out.print("Entered string : " + sentence);
   }
